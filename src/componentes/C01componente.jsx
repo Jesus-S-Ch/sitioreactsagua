5966379e3489569f3acad5f5be04e2500bfd0b4d
Jesus-S-Ch <jesussagua123@gmail.com>
2022-11-09 16:53:02 -0500

    primera ves que estoy accediendo a esta version

---
+++ src/componentes/C01componente.jsx
@@ -0,0 +1,12 @@
+//rafce
+import React from 'react'
+
+const C01componente = () => {
+  return (
+    <div>
+        Componenete definida con funcion flecha (rafce)
+    </div>
+  )
+}
+
+export default C01componente
